f0763209b05097149baf2e5815d7a1dad026954e
Wangchou Lu <kindalu@gmail.com>
2019-10-09 14:33:49 +0800

    add icon and content farm list by submodule

---
+++ app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chin
+//
+//  Created by Wangchou Lu on R 1/10/09.
+//  Copyright © Reiwa 1 com.wcl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ blocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  blocker
+//
+//  Created by Wangchou Lu on R 1/10/09.
+//  Copyright © Reiwa 1 com.wcl. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
